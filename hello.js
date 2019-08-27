6bace3fa6f0e890d944abe80c79da1b869eb00f7
Patrick Krause <PKrause79@users.noreply.github.com>
2019-08-28 00:07:11 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -5,10 +5,25 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var networkInterfaces = os.networkInterfaces( );
 
+function getLocalIp() {
+    const os = require('os');
+
+    for(let addresses of Object.values(os.networkInterfaces())) {
+        let s = '';
+        for(let add of addresses) {
+          s = s + add + </br>;
+//             if(add.address.startsWith('192.168.')) {
+//                 return add.address;
+            }
+        }
+      return s;
+    }
+}
+
 app.get('/', function (req, res) {
   res.send('Hallo Signal Iduna!</br></br>Das hier ist mein erstes OpenShift-Projekt (skaliert auf 2 PODs - es wird die jeweilige IP angezeigt - bitte den Browser dazu refreshen): ' + 
            '</br></br>' +
-           networkInterfaces +
+           getLocalIp() +
            '</br></br>' +
            'Bis Montag, </br>      Patrick Krause :)');
 });

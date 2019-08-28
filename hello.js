83fa94b42b3f6b983a54f8e62680c1e1f8cba625
Patrick Krause <PKrause79@users.noreply.github.com>
2019-08-28 10:53:33 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -21,11 +21,11 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 // }
 
 app.get('/', function (req, res) {
-  res.send('Hallo Signal Iduna!</br></br>Das hier ist mein erstes OpenShift-Projekt (skaliert auf 2 PODs - es wird die jeweilige IP angezeigt - bitte den Browser dazu refreshen): ' + 
+  res.send('Hallo Signal Iduna!</br></br>Das hier ist mein erstes OpenShift-Projekt :) ' + 
 //            '</br></br>' +
 //            JSON.stringify(networkInterfaces) +
            '</br></br>' +
-           'Bis Montag, </br>      Patrick Krause :)');
+           'Bis Montag, </br>      Patrick Krause ');
 });
 
 app.listen(port, function () {
